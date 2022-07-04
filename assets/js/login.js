1d83dcfea8f14b53fe3cef44370aa812ef91f8ed
rochmadnf <rochmadnf@gmail.com>
2022-07-04 16:21:55 +0800

    fix login page

--- assets/js/login.js
+++ assets/js/login.js
@@ -1,24 +1,29 @@
 "use strict"
 
-const loginForm = document.forms.LoginForm
-
-loginForm.addEventListener('submit', e => {
-  e.preventDefault();
-  const formData = new FormData(loginForm)
-  const users = JSON.parse(localStorage.getItem('users'))
+if(userLogin) {
+  console.clear()
+  location.href = `${location.protocol}//${location.host}`
+} else {
+  const loginForm = document.forms.LoginForm
   
-  // search user by username
-  const validation = users.find(user => user.username === formData.get('username'))
-
-  if (validation) {
-    if(validation.password === formData.get('password')) {
-      sessionStorage.setItem('current_user', JSON.stringify(validation));
-      alert(`Selamat, Anda berhasil masuk`);
-      location.href = `${location.protocol}//${location.host}`
+  loginForm.addEventListener('submit', e => {
+    e.preventDefault();
+    const formData = new FormData(loginForm)
+    const users = JSON.parse(localStorage.getItem('users'))
+    
+    // search user by username
+    const validation = users.find(user => user.username === formData.get('username'))
+  
+    if (validation) {
+      if(validation.password === formData.get('password')) {
+        sessionStorage.setItem('current_user', JSON.stringify(validation));
+        alert(`Selamat, Anda berhasil masuk`);
+        location.href = `${location.protocol}//${location.host}`
+      }else {
+        alert(`Nama Pengguna atau Katasandi Salah`)
+      }
     }else {
       alert(`Nama Pengguna atau Katasandi Salah`)
     }
-  }else {
-    alert(`Nama Pengguna atau Katasandi Salah`)
-  }
-})
+  })
+}
