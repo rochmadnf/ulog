b41784ab39fd69812d3d1dc35687ff03afe887f3
rochmadnf <rochmadnf@gmail.com>
2022-07-04 18:46:00 +0800

    add page register

---
+++ assets/js/register.js
@@ -0,0 +1,32 @@
+"use strict"
+
+if (userLogin) {
+    console.clear()
+    location.href = `${location.protocol}//${location.host}`
+} else {
+    const regisForm = document.forms.RegisterForm
+
+    regisForm.addEventListener('submit', e => {
+        e.preventDefault();
+        const formData = new FormData(regisForm)
+        const users = JSON.parse(localStorage.getItem('users'))
+
+        // search user by username
+        const validation = users.find(user => user.username === formData.get('username') || user.email === formData.get('email'))
+
+        if (validation) {
+            alert(`Nama Pengguna atau Surel telah terdaftar.`)
+        } else {
+            users.push({
+                "id": new Date().getTime(),
+                "name": formData.get('name'),
+                "username": formData.get('username'),
+                "email": formData.get('email'),
+                "password": formData.get('password'),
+            });
+
+            localStorage.setItem('users', JSON.stringify(users));
+            alert(`Selamat, Akun anda berhasil didaftarkan!`)
+        }
+    })
+}
