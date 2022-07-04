656d04e119fe38ee68bbe5ddb427d38403a058ec
rochmadnf <rochmadnf@gmail.com>
2022-07-04 18:53:50 +0800

    fix list article when 0 article user

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,25 +1,25 @@
 let articles;
-if(userLogin){
-  articles = JSON.parse(localStorage.getItem('articles')).filter(article => article.user_id === userLogin.id);
+if (userLogin) {
+    articles = JSON.parse(localStorage.getItem('articles')).filter(article => article.user_id === userLogin.id);
 } else {
-  alert("Silakan masuk terlebih dahulu")
-  location.href = `${location.protocol}//${location.host}/pages/login.html`
+    alert("Silakan masuk terlebih dahulu")
+    location.href = `${location.protocol}//${location.host}/pages/login.html`
 }
 
 const timeConvert = (unix) => {
-  const dateTime = new Date(unix);
-  return (new Intl.DateTimeFormat('id-Id', { dateStyle: 'full', timeStyle: 'long' }).format(dateTime))
+    const dateTime = new Date(unix);
+    return (new Intl.DateTimeFormat('id-Id', { dateStyle: 'full', timeStyle: 'long' }).format(dateTime))
 }
 
 const authorSearch = (authorId) => {
-  const users = JSON.parse(localStorage.getItem('users'));
+    const users = JSON.parse(localStorage.getItem('users'));
 
-  const author = users.find(user => user.id === authorId)
-  return author.name;
+    const author = users.find(user => user.id === authorId)
+    return author.name;
 }
 
 const Article = (props) => {
-  return `
+    return `
   <article class="article">
     <h1>${props.title}</h1>
     <p>
@@ -38,12 +38,20 @@ const Article = (props) => {
   `
 }
 
-if(articles) {
-  articles.map(
-    article => {
-      document.querySelector(".content").insertAdjacentHTML('beforeend', 
-      Article(article)
-      );
+if (articles) {
+    if (articles.length > 0) {
+        articles.map(
+            article => {
+                document.querySelector(".content").insertAdjacentHTML('beforeend',
+                    Article(article)
+                );
+            }
+        )
+    } else {
+        document.querySelector(".content").insertAdjacentHTML('beforeend',
+            `<div class="alert alert-info">
+          Kamu belum membuat satupun blog
+        </div>`
+        );
     }
-  )
 }
